15d7a17ca9fbfd511a35e88b9eb6e90b3c7918d1
AFK101 <cloudtoilet@gmail.com>
2018-11-14 21:08:51 +0100

    fillit.c

--- fillit.c
+++ fillit.c
@@ -25,11 +25,16 @@ int        check_map(char **line)
 
     while (line[y])
     {
+    	x = 0;
         while (line[y][x] && (line[y][x] == '.' || line[y][x] == '#'))
         {
-            if ((line[y - 1][x] == '#' && y > 0) ||
-             (line [y][x - 1] == '#' && x > 0) ||
-             (line[y][x + 1] == '#' && x < 4) || (line[y + 1][x] == '#' && y < 4))
+            if (line[y][x] == '#' && (line[y - 1][x] == '#' && y > 0))
+            	link_counter++;
+            if (line[y][x] == '#' && (line [y][x - 1] == '#' && x > 0))
+            	link_counter++;
+            if (line[y][x] == '#' && (line[y][x + 1] == '#' && x < 4))
+            	link_counter++;
+            if (line[y][x] == '#' && (line[y + 1][x] == '#' && y < 4))
                 link_counter++;
             x++;
         }
